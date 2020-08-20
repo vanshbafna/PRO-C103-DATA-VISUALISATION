d3fa30620a624bfdeeeb1ce14c9c4facaf233a61
vanshbafna <59309266+vanshbafna@users.noreply.github.com>
2020-08-20 20:00:06 +0530

    Update coronaGraph.py

--- coronaGraph.py
+++ coronaGraph.py
@@ -2,3 +2,4 @@ import plotly.express as px
 import pandas as pd
 df = pd.read_csv("D:/projects_vansh/module3/PROJ103/data.csv")
 fig = px.line(df,x="country",y = "cases",color = "date",title = "Per Capita Income")
+fig.show
